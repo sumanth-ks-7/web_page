32b3baa73f63cec6d4772857aa62e288eefca855
sumanth-ks-7 <sumanth.kodanakatte@gmail.com>
2022-07-05 08:58:37 +0200

    libraries added in main file

--- main.py
+++ main.py
@@ -2,15 +2,30 @@ from __init__ import app
 from flask import render_template
 
 
+posts = [
+    {
+        'author': 'Sumanth',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'Jan 1st, 2022'
+    },
+    {
+        'author': 'SSBHAT',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'Jan 2nd, 2022'
+    }
+]
+
 # @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == "__main__":

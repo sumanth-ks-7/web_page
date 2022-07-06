9123c5850bfe0a3ac8f0921ee7d406e0c696611b
sumanth-ks-7 <sumanth.kodanakatte@gmail.com>
2022-07-06 14:21:41 +0200

    added forms.py file and developped codes for front end

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from __init__ import app
-from flask import render_template, url_for
+from flask import render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
+app.config['SECRET_KEY'] = 'fd8740f98ddd013ce021f5f2bde3c3d7'  # to get secret key in python console type --> import secrets --> secrets.token_hex(16)
 
 posts = [
     {
@@ -27,6 +29,21 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == "__main__":
